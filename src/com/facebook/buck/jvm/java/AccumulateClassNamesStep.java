a57d90322372724862486e613dfc4e1b8f49e8f8
Nick <nick@bird.co>
2019-04-21 18:11:14 -0700

    wip skip check

--- src/com/facebook/buck/jvm/java/AccumulateClassNamesStep.java
+++ src/com/facebook/buck/jvm/java/AccumulateClassNamesStep.java
@@ -40,7 +40,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.lang.RuntimeException;
 
 /**
  * {@link Step} that takes a directory or zip of {@code .class} files and traverses it to get the
@@ -164,12 +163,12 @@ public class AccumulateClassNamesStep implements Step {
 
     for (String line : lines) {
       List<String> parts = CLASS_NAME_AND_HASH_SPLITTER.splitToList(line);
-      if (parts.size() != 2) {
-        throw new RuntimeException(String.join(",", parts));
-      }
+      // if (parts.size() != 2) {
+      //   throw new RuntimeException(String.join(",", parts));
+      // }
       // Preconditions.checkState(parts.size() == 2);
       String key = parts.get(0);
-      HashCode value = HashCode.fromString(parts.get(1));
+      HashCode value = HashCode.fromString(parts.get(parts.size() - 1));
       HashCode existing = classNames.putIfAbsent(key, value);
       if (existing != null && !existing.equals(value)) {
         throw new IllegalArgumentException(
