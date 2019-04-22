a3a96c96ba8ad1bb221dc82be92cfbf605514235
Nick <nick@bird.co>
2019-04-21 18:22:34 -0700

    wip

--- src/com/facebook/buck/jvm/java/AccumulateClassNamesStep.java
+++ src/com/facebook/buck/jvm/java/AccumulateClassNamesStep.java
@@ -163,14 +163,14 @@ public class AccumulateClassNamesStep implements Step {
 
     for (String line : lines) {
       List<String> parts = CLASS_NAME_AND_HASH_SPLITTER.splitToList(line);
-            String hash = parts.get(parts.size() - 1);
+      String hash = parts.get(parts.size() - 1);
       String key;
       if (parts.size() != 2) {
           key = String.join(",", parts.subList(0, parts.size() - 2));
       } else {
           key = parts.get(0);
       }
-      HashCode value = HashCode.fromString(parts.get(parts.size() - 1));
+      HashCode value = HashCode.fromString(hash);
       HashCode existing = classNames.putIfAbsent(key, value);
       if (existing != null && !existing.equals(value)) {
         throw new IllegalArgumentException(
